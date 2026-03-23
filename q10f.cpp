e83e31fbec80fd8fcecbd73c8a3c75e809d94772
Ansh Verma <anshv6256@gmail.com>
2026-03-23 22:26:46 +0530

    Add callCounter function to count function calls

---
+++ q10f.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void callCounter() {
+    static int count = 0; 
+    count++;
+    cout << "Function called " << count << " times" << endl;
+}
+
+int main() {
+    callCounter();
+    callCounter();
+    callCounter();
+    callCounter();
+    callCounter();
+    callCounter();
+    callCounter();
+
+    return 0;
+}
